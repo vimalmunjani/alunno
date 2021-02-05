4f2d7ce5a6d45ee45617137f0cdf73dec1aa49bc
Vimal Munjani <vimal.j.munjani@gmail.com>
2021-02-05 20:33:06 +0530

    Add: (server) static path to client dist

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,15 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
 
 // creating the express app
 const app = express();
 
 // using the middlewares
 app.use(bodyParser.json());
+
+// set the static path to serve client app
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 // test route
 app.use('/ping', (request: Request, response: Response) => {
