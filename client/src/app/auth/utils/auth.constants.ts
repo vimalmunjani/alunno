4bb33f0f683e0266de79a7cf6208cd3adf999abb
Vimal Munjani <vimal.j.munjani@gmail.com>
2021-02-08 06:17:19 +0530

    Add: (client) auth utils

---
+++ client/src/app/auth/utils/auth.constants.ts
@@ -0,0 +1,5 @@
+export class AuthConstants {
+    public static readonly AUTH_API_PATH = '/api/auth'
+    public static readonly SIGN_UP_URL = `${AuthConstants.AUTH_API_PATH}/signup`;
+    public static readonly SIGN_IN_URL = `${AuthConstants.AUTH_API_PATH}/signin`;
+}
